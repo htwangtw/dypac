236ef75284ecf37b0730f0e8bcd7847a950f6f30
Pierre Bellec <pierre.bellec@gmail.com>
2019-05-09 10:04:52 -0400

    Added new applications: faces and fMRI.

--- sca/__init__.py
+++ sca/__init__.py
@@ -1,7 +1,7 @@
 """
 Recursive clustering of clusters based on subsamples
 """
-from .sca import _part2onehot, _select_subsample, _replicate_cluster,
+from .sca import _part2onehot, _select_subsample, _replicate_cluster, \
     recursive_cluster
 __all__ = ['_part2onehot', '_select_subsample', '_replicate_cluster',
            'recursive_cluster']
