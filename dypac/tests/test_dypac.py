681742b6bb227b2f57b4fd512b8081febf95b267
Hanads <hanadsharmarke@gmail.com>
2020-02-26 16:44:52 -0500

    part2onehot 7 tests

--- dypac/tests/test_dypac.py
+++ dypac/tests/test_dypac.py
@@ -1,101 +1,91 @@
 import numpy as np
 import dypac
 import pytest
-
-test_one = dict()
-test_one["part"] = np.array([[2, 1, 1], [3, 1, 1]])
-test_one["clusters"] = 5
-test_one["one_hot"] = [
-    [False, False, False],
-    [False, True, True],
-    [True, False, False],
-    [False, False, False],
-    [False, False, False],
-    [False, False, False],
-    [False, True, True],
-    [False, False, False],
-    [True, False, False],
-    [False, False, False],
-]
-
-test_two = dict()
-test_two["part"] = np.array([[15, 7], [12, 17]])
-test_two["clusters"] = 20
-test_two["one_hot"] = [
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, True],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [True, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [True, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, False],
-    [False, True],
-    [False, False],
-    [False, False],
-]
-
-
-test_three = dict()
-test_three["part"] = np.array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]])
-test_three["clusters"] = 5
-test_three["one_hot"] = [
-    [False, False, False, False],
-    [True, True, True, True],
-    [False, False, False, False],
-    [False, False, False, False],
-    [False, False, False, False],
-    [False, False, False, False],
-    [False, False, False, False],
-    [True, True, True, True],
-    [False, False, False, False],
-    [False, False, False, False],
-    [False, False, False, False],
-    [False, False, False, False],
-    [False, False, False, False],
-    [True, True, True, True],
-    [False, False, False, False],
-]
+import pickle
 
 
 def test_part2onehot():
+    test = dict()
+    test["part"] = np.array([[2, 1, 1], [3, 1, 1]])
+    test["clusters"] = 5
+    test["one_hot"] = [
+        [False, False, False],
+        [False, True, True],
+        [True, False, False],
+        [False, False, False],
+        [False, False, False],
+        [False, False, False],
+        [False, True, True],
+        [False, False, False],
+        [True, False, False],
+        [False, False, False],
+    ]
 
-    dypac_onehot = dypac._part2onehot(test_one["part"], test_one["clusters"]).toarray()
-    assert np.array_equal(dypac_onehot, test_one["one_hot"])
+    dypac_onehot = dypac._part2onehot(test["part"], test["clusters"]).toarray()
+    assert np.array_equal(dypac_onehot, test["one_hot"])
+    return 0
 
-    dypac_onehot2 = dypac._part2onehot(test_two["part"], test_two["clusters"]).toarray()
-    assert np.array_equal(dypac_onehot2, test_two["one_hot"])
 
-    dypac_onehot3 = dypac._part2onehot(
-        test_three["part"], test_three["clusters"]
-    ).toarray()
-    assert np.array_equal(dypac_onehot3, test_three["one_hot"])
+def test2_part2onehot():
+    test = dict()
+    test["part"] = np.array([[0]])
+    test["clusters"] = 5
+    test["one_hot"] = [[True], [False], [False], [False], [False]]
+
+    dypac_onehot = dypac._part2onehot(test["part"], test["clusters"]).toarray()
+    assert np.array_equal(dypac_onehot, test["one_hot"])
+
+
+def test3_part2onehot():
+    test = dict()
+    test["part"] = np.array([[100]])
+    test["clusters"] = 99
+
+    with pytest.raises(ValueError, match="row index exceeds matrix dimensions"):
+        dypac_onehot = dypac._part2onehot(test["part"], test["clusters"]).toarray()
+
+
+def test4_part2onehot():
+    test = dict()
+    test["part"] = np.array([[]])
+    test["clusters"] = 1
+    test["one_hot"] = [[]]
+
+    dypac_onehot = dypac._part2onehot(test["part"], test["clusters"]).toarray()
+    assert np.array_equal(dypac_onehot, test["one_hot"])
+
+
+def test5_part2onehot():
+    test = dict()
+    test["part"] = np.array([[-1]])
+    test["clusters"] = 1
+
+    with pytest.raises(ValueError, match="negative row index found"):
+        dypac_onehot = dypac._part2onehot(test["part"], test["clusters"]).toarray()
+
+
+def test6_part2onehot():
+    test = dict()
+    test["part"] = np.array([1])
+    test["clusters"] = 10
+
+    with pytest.raises(ValueError, match="expected 2, got 1"):
+        dypac_onehot = dypac._part2onehot(test["part"], test["clusters"]).toarray()
+
+
+def test7_part2onehot():
+    test = dict()
+    test["part"] = np.array([[True, False]])
+    test["clusters"] = 5
+
+    test["one_hot"] = [
+        [False, True],
+        [True, False],
+        [False, False],
+        [False, False],
+        [False, False],
+    ]
+
+    dypac_onehot = dypac._part2onehot(test["part"], test["clusters"]).toarray()
+
+    assert np.array_equal(dypac_onehot, test["one_hot"])
