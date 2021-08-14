fa3983fe15223b53814fce9ccc641b7799154ad0
Pierre Bellec <pierre.bellec@gmail.com>
2021-08-14 16:57:55 -0400

    Added a new module to apply bascpp to images (png, etc). WIP.

--- dypac/__init__.py
+++ dypac/__init__.py
@@ -1,6 +1,7 @@
 """Dynamic Parcel Aggregation with Clustering (dypac)."""
 from dypac.dypac import Dypac
+from dypac.impac import Impac
 from dypac.embeddings import Embedding
 from dypac.bascpp import replicate_clusters, find_states, stab_maps
 from dypac.tests import test_bascpp, test_dypac
-__all__ = ['Dypac', 'test_bascpp', 'test_dypac', 'replicate_clusters', 'find_states', 'stab_maps', 'Embedding']
+__all__ = ['Dypac', 'Impac', 'test_bascpp', 'test_dypac', 'replicate_clusters', 'find_states', 'stab_maps', 'Embedding']
