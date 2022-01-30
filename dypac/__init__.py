836ae1575cad8def4f1618c76b24e41d67b2f227
Pierre Bellec <pierre.bellec@gmail.com>
2022-01-30 00:33:34 -0500

    Removing the impac module from imports.

--- dypac/__init__.py
+++ dypac/__init__.py
@@ -1,6 +1,5 @@
 """Dynamic Parcel Aggregation with Clustering (dypac)."""
 from dypac.dypac import Dypac
-from dypac.impac import Impac
 from dypac.embeddings import Embedding
 from dypac.bascpp import replicate_clusters, find_states, stab_maps
 from dypac.tests import test_bascpp, test_dypac
@@ -8,7 +7,6 @@ from dypac.masker import LabelsMasker, MapsMasker
 
 __all__ = [
     "Dypac",
-    "Impac",
     "test_bascpp",
     "test_dypac",
     "replicate_clusters",
